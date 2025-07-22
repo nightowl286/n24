e529736d8426f740849cf0092d6c5c27dbe1a851
Nightowl <nightowl286@protonmail.com>
2025-07-22 22:55:46 +0100

    * Modify project to use a build script for the website.

---
+++ build_site.sh
@@ -0,0 +1,26 @@
+# Ensure output directory is empty
+rm -rf site_output
+mkdir site_output
+
+
+# Copy over external code files
+cp -r src/site/external site_output
+
+
+# Copy over static files
+cp -r src/site/data site_output
+cp src/site/survey.md site_output
+
+
+# Copy over styles
+cp src/site/*.css site_output
+
+
+# Copy over code files
+cp src/site/*.js site_output
+
+
+# Copy over html files
+cp src/site/index.html site_output
+cp src/site/survey.html site_output
+cp src/site/case-studies.html site_output
