4fa366a85d82ea98940dde6327582dd1eb189342
Nightowl <nightowl286@protonmail.com>
2025-07-23 00:52:31 +0100

    * Improve build script. * Made the deploy GH action react to the build script changing.

--- build_site.sh
+++ build_site.sh
@@ -1,26 +1,30 @@
-# Ensure output directory is empty
+#!/bin/bash
+set -e
+
+
+printf "[*] Ensure output directory is empty\n"
 rm -rf site_output
 mkdir site_output
 
 
-# Copy over external code files
-cp -r src/site/external site_output
+printf "[*] Copy over external code files\n"
+cp -rv src/site/external site_output
 
 
-# Copy over static files
-cp -r src/site/data site_output
-cp src/site/survey.md site_output
+printf "\n[*] Copy over static files\n"
+cp -rv src/site/data site_output
+cp -v src/site/survey.md site_output
 
 
-# Copy over styles
-cp src/site/*.css site_output
+printf "\n[*] Copy over styles\n"
+cp -v src/site/*.css site_output
 
 
-# Copy over code files
-cp src/site/*.js site_output
+printf "\n[*] Copy over code files\n"
+cp -v src/site/*.js site_output
 
 
-# Copy over html files
-cp src/site/index.html site_output
-cp src/site/survey.html site_output
-cp src/site/case_studies.html site_output
+printf "\n[*] Copy over html files\n"
+cp -v src/site/index.html site_output
+cp -v src/site/survey.html site_output
+cp -v src/site/case_studies.html site_output
